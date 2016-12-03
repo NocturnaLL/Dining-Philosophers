1fe446d39fb6cd1f02ba394b1244fafd2244d892
NocturnaLL <bfidans1@binghamton.edu>
2016-12-04 02:58:57 +0300

    Number of seat, delay(usleep) parts are missing

---
+++ dining.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+typedef struct {
+  int position;
+  int count;
+  sem_t *forks;
+  sem_t *lock;
+} params_t;
+
+void initialize_semaphores(sem_t *lock, sem_t *forks, int num_forks);
+void run_all_threads(pthread_t *threads, sem_t *forks, sem_t *lock, int num_philosophers);
+
+void *philosopher(void *params);
+void think(int position);
+void eat(int position);
+pthread_mutex_t mutex;
+int main(int argc, char *argv[])
+{
+
+
+  int num_philosophers = atoi(argv[1]);
+
+  sem_t lock;
+  sem_t forks[num_philosophers];
+  pthread_t philosophers[num_philosophers];
+
+  initialize_semaphores(&lock, forks, num_philosophers);
+  run_all_threads(philosophers, forks, &lock, num_philosophers);
+  pthread_exit(NULL);
+}
+
+void initialize_semaphores(sem_t *lock, sem_t *forks, int num_forks)
+{
+  int i;
+  for(i = 0; i < num_forks; i++) {
+    sem_init(&forks[i], 0, 1);
+  }
+
+  sem_init(lock, 0, num_forks - 1);
+}
+
+void run_all_threads(pthread_t *threads, sem_t *forks, sem_t *lock, int num_philosophers)
+{
+  int i;
+  for(i = 0; i < num_philosophers; i++) {
+    params_t *arg = malloc(sizeof(params_t));
+    arg->position = i;
+    arg->count = num_philosophers;
+    arg->lock = lock;
+    arg->forks = forks;
+
+    pthread_create(&threads[i], NULL, philosopher, (void *)arg);
+  }
+}
+
+void *philosopher(void *params)
+{
+  int i;
+  params_t self = *(params_t *)params;
+
+  for(i = 0; i < 3; i++) {
+    think(self.position);
+
+    //sem_wait(self.lock);
+    pthread_mutex_lock(&mutex);
+    sem_wait(&self.forks[self.position]);
+    sem_wait(&self.forks[(self.position + 1) % self.count]);
+    eat(self.position);
+    sem_post(&self.forks[self.position]);
+    sem_post(&self.forks[(self.position + 1) % self.count]);
+    pthread_mutex_unlock(&mutex);
+    //sem_post(self.lock);
+
+  }
+
+  think(self.position);
+  pthread_exit(NULL);
+}
+
+void think(int position)
+{
+  printf("Philosopher %d thinking...\n", position);
+}
+
+void eat(int position)
+{
+  printf("Philosopher %d eating...\n", position);
+}
